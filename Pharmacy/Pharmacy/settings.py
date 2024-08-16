86984b5510eb99aec67a9c65144ee3fbd17ff007
vaibhavvagrawall <vaibhav.18.agrawal@gmail.com>
2024-08-16 22:24:06 +0530

    Troubleshooting Error
    Switching the database to mysql as djongo was giving compatibility error.
    
    Also deleted the project1 as virtual env. Inserted a virtual env inside the Pharmacy directory with the name pharmaenv

--- Pharmacy/Pharmacy/settings.py
+++ Pharmacy/Pharmacy/settings.py
@@ -76,28 +76,16 @@ WSGI_APPLICATION = 'Pharmacy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'pharmacy',
-#        'USER': 'root',
-#        'PASSWORD': 'bANSAL1809',
-#        'HOST':'localhost',
-#        'PORT':'3306',
-#    }
-#}
-
 DATABASES = {
-    'default':{
-        'ENGINE': 'djongo',
-        'NAME': 'pharmacyinv',
-        'CLIENT': {
-            'host': 'localhost',
-            'port': 27017,
-        }
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'pharmacy',
+       'USER': 'root',
+       'PASSWORD': 'bANSAL1809',
+       'HOST':'localhost',
+       'PORT':'3306',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
