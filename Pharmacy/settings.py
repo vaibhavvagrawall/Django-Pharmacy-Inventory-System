148a32c41b0a24beba2ccf595191070dee3efd16
vaibhavvagrawall <vaibhav.18.agrawal@gmail.com>
2024-08-18 01:40:56 +0530

    static file problem

--- Pharmacy/settings.py
+++ Pharmacy/settings.py
@@ -125,7 +125,10 @@ EMAIL_HOST_PASSWORD = 'niji ghpl ctlc tikq'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+import os
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -138,7 +141,3 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL = '/dashboard'
 LOGIN_URL = 'login'
-
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
