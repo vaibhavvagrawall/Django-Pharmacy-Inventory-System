d71e6db42f17d769f4e42bcd9978db028d64341c
vaibhavvagrawall <vaibhav.18.agrawal@gmail.com>
2024-08-17 23:41:51 +0530

    Miscellaneous

--- Pharmacy/Pharmacy/settings.py
+++ Pharmacy/Pharmacy/settings.py
@@ -126,6 +126,7 @@ EMAIL_HOST_PASSWORD = 'niji ghpl ctlc tikq'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
