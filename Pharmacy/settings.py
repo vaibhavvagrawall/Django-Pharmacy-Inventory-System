33f23cacd17ce906f48a151446aec46190069acd
vaibhavvagrawall <vaibhav.18.agrawal@gmail.com>
2024-08-18 01:35:04 +0530

    Static file prblem

--- Pharmacy/settings.py
+++ Pharmacy/settings.py
@@ -126,10 +126,6 @@ EMAIL_HOST_PASSWORD = 'niji ghpl ctlc tikq'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
