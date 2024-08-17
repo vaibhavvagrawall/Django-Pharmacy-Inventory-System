e33dd06607a694327fd94079858725ea64951c7c
vaibhavvagrawall <vaibhav.18.agrawal@gmail.com>
2024-08-18 03:29:27 +0530

    sqlite3 to mysql

--- Pharmacy/settings.py
+++ Pharmacy/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'Pharmacy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PHARMACY_INVENTORY',
+        'USER': 'root',
+        'PASSWORD': 'bANSAL1809',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- pharmaenv/Scripts/activate.bat
+++ pharmaenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\DELL\OneDrive\Documents\GitHub\Django-Pharmacy-Inventory-System\Pharmacy\pharmaenv
+set VIRTUAL_ENV=C:\Users\DELL\OneDrive\Documents\GitHub\Django-Pharmacy-Inventory-System\pharmaenv
 
 if not defined PROMPT set PROMPT=$P$G
 
