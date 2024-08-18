f3f1f3a713acdefa572252999966f96fcb1dc521
vaibhavvagrawall <vaibhav.18.agrawal@gmail.com>
2024-08-18 14:50:58 +0530

    syntax solve

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
-echo("BUILD START")
+echo "BUILD START"
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py makemigrations
 python3.12 manage.py migrate 
 python3.12 manage.py collectstatic --noinput --clear
-echo("BUILD END")
+echo "BUILD END"
